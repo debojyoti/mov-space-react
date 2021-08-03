7dac6088b47da4d400da41e2c322bf8e9c5f7d54
debojyoti1 <debojyoti.js@gmail.com>
2021-08-04 03:32:50 +0530

    UPdated search logic

--- src/components/search-bar/search-bar.jsx
+++ src/components/search-bar/search-bar.jsx
@@ -1,23 +1,57 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import "./search-bar.scss";
 
 const SearchBar = (props) => {
-    return (  
-        <div className="searchOuterWRapper searchActive">
-                    <div className="searchInnerWrapper">
-                    <div className="searchBar">
-                        <input type="text" placeholder="Search" autoFocus />
-                        <div className="typeSelector">
-                            <select name="" id="">
-                                <option value="">All</option>
-                                <option value="">Movies</option>
-                                <option value="">Series</option>
-                            </select>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-    );
-}
- 
+  const { clearResults, triggerNewSearch } = props;
+
+  const [searchText, setSearchText] = useState("");
+  const [searchType, setSearchType] = useState("all");
+
+  useEffect(() => {
+    const trimmedSearchedText = searchText.trim();
+    if (!trimmedSearchedText?.length) {
+      clearResults();
+    } else {
+      const searchParams = {
+        s: searchText,
+      };
+      if (searchType !== "all") {
+        searchParams.type = searchType;
+      }
+      triggerNewSearch(searchParams);
+    }
+  }, [searchText, searchType]);
+
+  return (
+    <div
+      className={`searchOuterWRapper ${searchText?.length && "searchActive"}`}
+    >
+      <div className="searchInnerWrapper">
+        <div className="searchBar">
+          <input
+            className={searchText?.length && "hasText"}
+            type="text"
+            placeholder="Search"
+            autoFocus
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <div className="typeSelector">
+            <select
+              name="typeSelector"
+              id="typeSelector"
+              onChange={(e) => setSearchType(e.target.value)}
+            >
+              <option value="all">All</option>
+              <option value="movie">Movies</option>
+              <option value="series">Series</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default SearchBar;
--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import Header from '../../components/header/header';
-import MovieBrowser from '../../components/movie-browser/movie-browser';
-import SearchBar from '../../components/search-bar/search-bar';
+import React, { useEffect, useState } from "react";
+import Header from "../../components/header/header";
+import MovieBrowser from "../../components/movie-browser/movie-browser";
+import SearchBar from "../../components/search-bar/search-bar";
 import "./home-page.scss";
 
-const HomePage = () => {
-    return (  
-        <>
-            <div className="homePageWrapper">
-                <Header />
-                <SearchBar />
-                {/* <div className="searchPrompt">
-                    <p>Search your favourite titles</p>
-                </div> */}
-                {/* <div className="searchInProgress">
-                    <img src={require("../../assets/images/mov-search.gif")} alt="" />
-                    <h3>Search results are on the way</h3>
-                </div> */}
-                <MovieBrowser />
-                </div>
-        </>
-    );
-}
- 
+const HomePage = (props) => {
+  const [searchParams, setSearchParams] = useState({});
+  const [isSearchIng, setIsSearchIng] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [currentPaginationIndex, setCurrentPaginationIndex] = useState(0);
+  const [maxPaginationPageCount, SetmaxPaginationPageCount] = useState(0);
+
+  const _clearResults = () => {
+    setIsSearchIng(false);
+    setMovies([]);
+    setSearchParams({});
+  };
+
+  const _triggerNewSearch = (searchParams) => {
+    setIsSearchIng(true);
+    _processSearch(searchParams);
+  };
+
+  const _processSearch = async (searchParams) => {};
+
+  useEffect(() => {}, [searchParams]);
+
+  const _renderActiveView = () => {
+    if (isSearchIng) {
+      return (
+        <div className="searchInProgress">
+          <img src={require("../../assets/images/mov-search.gif")} alt="" />
+          <h3>Search results are on the way</h3>
+        </div>
+      );
+    } else if (!isSearchIng && !Object.keys(searchParams).length) {
+      return (
+        <div className="searchPrompt">
+          <p>Search your favourite titles</p>
+        </div>
+      );
+    } else if (!isSearchIng && movies.length) {
+      return <MovieBrowser />;
+    }
+  };
+
+  return (
+    <>
+      <div className="homePageWrapper">
+        <Header />
+        <SearchBar
+          clearResults={_clearResults}
+          triggerNewSearch={_triggerNewSearch}
+        />
+        {_renderActiveView()}
+      </div>
+    </>
+  );
+};
+
 export default HomePage;
